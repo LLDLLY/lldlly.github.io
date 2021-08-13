20c9d7dc1178648b8d5ca1ff904b21a29f294f4a
yaoli <18861821692@163.com>
2021-08-13 17:52:24 +0800

    update

--- js/self.js
+++ js/self.js
@@ -1,5 +1,1 @@
-// 魔改代码 js
-// var full_page = document.getElementsByClassName("full_page");
-// if (full_page.length != 0) {
-//   full_page[0].style.background = "transparent";
-// }
+"use strict";
